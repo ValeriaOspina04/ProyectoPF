aa51ed6446088bdf573a5bc8fbd64df2db6d6e8a
caguilardolca@gmail.com <vale-ospina2005@outlook.com>
2025-09-07 16:54:29 -0500

    commit con icono agregado

--- proyectopf/lib/main.dart
+++ proyectopf/lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,6 +26,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
